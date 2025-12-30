697400fdf9949af278e9381424f9a42157112a6e
DanZhu <danzhu.xrloong@gmail.com>
2025-12-30 19:42:09 +0800

    test: simplify tests by using fixture

--- tests/DanZhu/GTP/test_interpreter.py
+++ tests/DanZhu/GTP/test_interpreter.py
@@ -1,53 +1,43 @@
+from types import ModuleType
 import pytest
+
+@pytest.fixture
+def interpreter():
+    from DanZhu.GTP import interpreter
+    return interpreter
 
 def test_import_interpreter():
     from DanZhu.GTP import interpreter
 
-def test_interpret_empty_respose__with_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_empty_respose__with_id(interpreter: ModuleType):
     response = interpreter.interpretSuccess(response = "", id = 2397)
     assert response == "=2397"
 
-def test_interpret_empty_respose__without_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_empty_respose__without_id(interpreter: ModuleType):
     response = interpreter.interpretSuccess(response = "")
     assert response == "="
 
-def test_interpret_response__with_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_response__with_id(interpreter: ModuleType):
     response = interpreter.interpretSuccess(response = "test-response", id = 2397)
     assert response == "=2397 test-response"
 
-def test_interpret_response__without_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_response__without_id(interpreter: ModuleType):
     response = interpreter.interpretSuccess(response = "test-response")
     assert response == "= test-response"
 
-def test_interpret_failure__with_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_failure__with_id(interpreter: ModuleType):
     response = interpreter.interpretFailure(error = "test-error", id = 2397)
     assert response == "?2397 test-error"
 
-def test_interpret_failure__without_id():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_failure__without_id(interpreter: ModuleType):
     response = interpreter.interpretFailure(error = "test-error")
     assert response == "? test-error"
 
-def test_interpret_panic():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_panic(interpreter: ModuleType):
     response = interpreter.interpretPanic()
     assert response == "! panic"
 
-def test_interpret_response():
-    from DanZhu.GTP import interpreter
-
+def test_interpret_response(interpreter: ModuleType):
     strResponse = interpreter.interpretResponse("=1234 test-response")
     assert strResponse == "=1234 test-response\n\n"
 
