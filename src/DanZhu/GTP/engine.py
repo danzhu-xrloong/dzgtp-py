eced96c868a0da5c43604d5d31a0a8c7230ce739
DanZhu <danzhu.xrloong@gmail.com>
2025-12-30 21:14:11 +0800

    feat: implement basic archtecture for GtpEngine

---
+++ src/DanZhu/GTP/engine.py
@@ -0,0 +1,80 @@
+from . import interpreter
+from contextlib import redirect_stdout
+from contextlib import contextmanager
+
+@contextmanager
+def redirect_stdin(new_stdin):
+    import sys
+
+    old_stdin = sys.stdin
+    try:
+        sys.stdin = new_stdin
+        yield
+    finally:
+        sys.stdin = old_stdin
+
+class GtpEngine:
+    PROTOCOL_VERSION = 2
+
+    def __init__(self, streamIn, streamOut):
+        self.streamIn = streamIn
+        self.streamOut = streamOut
+
+    def input(self) -> str:
+        line = self.streamIn.readline()
+        return line
+
+    def output(self, message: str):
+        self.streamOut.write(interpreter.interpretResponse(message))
+        self.streamOut.flush()
+
+    def outputSuccess(self, response: str, id: int | None):
+        message = interpreter.interpretSuccess(response, id = id)
+        self.output(message)
+
+    def outputFailure(self, error: str, id: int | None):
+        message = interpreter.interpretFailure(error, id = id)
+        self.output(message,)
+
+    def outputPanic(self):
+        message = interpreter.interpretPanic()
+        self.output(message)
+
+    def start(self):
+        with redirect_stdin(self.streamIn), redirect_stdout(self.streamOut):
+            while True:
+                line = self.input()
+                if len(line) == 0:
+                    break
+                isDone = self.interpretCommand(line)
+                if isDone:
+                    break
+
+    def interpretCommand(self, line):
+        # Remove comments
+        line = line.split('#')[0]
+        line = line.strip()
+        if len(line) == 0:
+            return
+        parts = line.split(' ')
+
+        id = None
+        try:
+            id = int(parts[0])
+            command = parts[1]
+            args = parts[2:]
+        except ValueError:
+            id = None
+            command = parts[0]
+            args = parts[1:]
+
+        if command == "quit":
+            self.outputSuccess("", id)
+            self.streamIn.close()
+            self.streamOut.close()
+            return True
+        if command == "protocol_version":
+            self.outputSuccess("{}".format(GtpEngine.PROTOCOL_VERSION), id)
+        else:
+            self.outputFailure("unknown command", id)
+
---
+++ tests/DanZhu/GTP/test_engine.py
@@ -0,0 +1,67 @@
+import pytest
+import io
+from typing import TypeAlias
+from DanZhu.GTP.engine import GtpEngine
+
+EngineHandler: TypeAlias = tuple[GtpEngine, io.TextIOWrapper, io.TextIOWrapper]
+
+@pytest.fixture
+def engineHandler():
+    import os
+    from DanZhu.GTP import engine
+
+    pipeOutsideToEngine = os.pipe()
+    pipeEngineToOutside = os.pipe()
+    streamOutsideToEngineReceiver = open(pipeOutsideToEngine[0], "r")
+    streamOutsideToEngineSender = open(pipeOutsideToEngine[1], "w")
+    streamEngineToOutsideReceiver = open(pipeEngineToOutside[0], "r")
+    streamEngineToOutsideSender = open(pipeEngineToOutside[1], "w")
+
+    e = engine.GtpEngine(streamIn = streamOutsideToEngineReceiver, streamOut = streamEngineToOutsideSender)
+    yield (e, streamOutsideToEngineSender, streamEngineToOutsideReceiver)
+
+    streamOutsideToEngineReceiver.close()
+    streamOutsideToEngineSender.close()
+    streamEngineToOutsideReceiver.close()
+    streamEngineToOutsideSender.close()
+
+def test_import_engine():
+    from DanZhu.GTP import engine
+
+def test_engine_output(engineHandler: EngineHandler):
+    engine, _, streamEngineToOutsideReceiver = engineHandler
+    engine.output(message = "test-message")
+    assert streamEngineToOutsideReceiver.readline() == "test-message\n"
+    assert streamEngineToOutsideReceiver.readline() == "\n"
+
+def test_engine_input(engineHandler: EngineHandler):
+    engine, streamOutsideToEngineSender, _ = engineHandler
+
+    streamOutsideToEngineSender.write("test-message\n")
+    streamOutsideToEngineSender.flush()
+    line = engine.input()
+    assert line == "test-message\n"
+
+@pytest.mark.parametrize(
+    argnames='command, expected',
+    argvalues=[
+        ("2397 protocol_version", "=2397 2"),
+        ("protocol_version", "= 2"),
+        ("2397 test-command", "?2397 unknown command"),
+        ("test-command", "? unknown command"),
+        ]
+)
+def test_interpretSuccess(
+        engineHandler: EngineHandler,
+        command: str,
+        expected: str
+    ):
+    engine, streamOutsideToEngineSender, streamEngineToOutsideReceiver = engineHandler
+
+    streamOutsideToEngineSender.write("{}\n".format(command))
+    streamOutsideToEngineSender.flush()
+    streamOutsideToEngineSender.close()
+    engine.start()
+    line = streamEngineToOutsideReceiver.readline()
+    assert line.strip() == expected
+
